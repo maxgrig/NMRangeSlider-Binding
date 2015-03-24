dc541d10560f5152857cbe3297cc8428071b515f
Max Grigoriev <themaxgrig@gmail.com>
2015-03-24 18:54:36 +0200

    Static library

---
+++ StaticLibrary/NMRangeSlider/NMRangeSlider/NMRangeSlider.h
@@ -0,0 +1,13 @@
+//
+//  NMRangeSlider.h
+//  NMRangeSlider
+//
+//  Created by Maksym Grygoriev on 3/24/15.
+//  Copyright (c) 2015 Maksym Grygoriev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NMRangeSlider : NSObject
+
+@end
---
+++ StaticLibrary/NMRangeSlider/NMRangeSlider/NMRangeSlider.m
@@ -0,0 +1,13 @@
+//
+//  NMRangeSlider.m
+//  NMRangeSlider
+//
+//  Created by Maksym Grygoriev on 3/24/15.
+//  Copyright (c) 2015 Maksym Grygoriev. All rights reserved.
+//
+
+#import "NMRangeSlider.h"
+
+@implementation NMRangeSlider
+
+@end
